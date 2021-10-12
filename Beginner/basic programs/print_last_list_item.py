f2ceaed90b73f587bb9280e89e1149d5d33f66c8
Sejalcodes <61181816+Sejalcodes@users.noreply.github.com>
2021-10-12 18:32:22 +0530

    Update print_last_list_item.py

--- Beginner/basic programs/print_last_list_item.py
+++ Beginner/basic programs/print_last_list_item.py
@@ -1,3 +1,9 @@
 #get a list from the user
-#print the last element in the list
+#print the last element in the list(Done)
 #you can use any method you are comfortable with 
+
+def fetch_last_item(items):
+  return items[-1]
+
+print(fetch_last_item([4, 6, 2, 8, 3]))
+print(fetch_last_item(["apple", "orange", "guava", "mango"]))
