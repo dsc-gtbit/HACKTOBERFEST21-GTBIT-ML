1685fb39a4097abb128c8abec8dd1db9dd5e3c68
Sejalcodes <61181816+Sejalcodes@users.noreply.github.com>
2021-10-29 14:35:28 +0530

    Update complete_insertion_sort.py

--- Beginner/searching and sorting/complete_insertion_sort.py
+++ Beginner/searching and sorting/complete_insertion_sort.py
@@ -2,4 +2,14 @@ def insertion_sort(InputList):
    for i in range(1, len(InputList)):
       j = i-1
       nxt_element = InputList[i]
+      
+      while j >= 0 and nxt_element < InputList[j]:
+            InputList[j + 1] = InputList[j]
+            j -= 1
+        InputList[j + 1] = nxt_element
+    return InputList
+
+    
+arr = [5,2,8,6,1]
+print(insertion_sort(arr))
 # Compare the current element with next one and write the subsequent code to complete insertion sort
